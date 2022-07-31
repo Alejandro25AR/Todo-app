b20aa9284254d39d1c0d54f74b190b886b8a8a01
Alejandro25AR <alejo.ave25@gmail.com>
2022-07-31 13:01:00 -0500

    change initial task id

--- javascript/helpers/validate_first_visit.js
+++ javascript/helpers/validate_first_visit.js
@@ -2,32 +2,32 @@ import { setDataLocalStorage,getDataLocalStorage } from "./local_storage.js";
 
 const initialTasks = [
   {
-    id: 'item1',
+    id: 'item0',
     text: 'Complete online JavaScript course',
     isCompleted: false
   },
   {
-    id: 'item2',
+    id: 'item1',
     text: 'Jog around the park x3',
     isCompleted: false
   },
   {
-    id: 'item3',
+    id: 'item2',
     text: '10 minutes meditation',
     isCompleted: false
   },
   {
-    id: 'item4',
+    id: 'item3',
     text: 'Read for 1 hour',
     isCompleted: false
   },
   {
-    id: 'item5',
+    id: 'item4',
     text: 'Pick up groceries',
     isCompleted: false
   },
   {
-    id: 'item6',
+    id: 'item5',
     text: 'Completed Todo App on Frontend Mentor',
     isCompleted: true
   }
